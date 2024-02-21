ed73672b50a78654e9dcd0f9d8e0fc35981c3cd1
Dev Flask <valtemir.procopio@gmail.com>
2024-02-21 11:58:21 -0300

    .

--- senac_manha/src/ts/exercicio1.ts
+++ senac_manha/src/ts/exercicio1.ts
@@ -1,4 +1,3 @@
-
 // Cadastro de Funcionários:
 // • Detalhes pessoais e profissionais dos colaboradores.
 class Funcionario {
@@ -7,6 +6,7 @@ class Funcionario {
     telefone: string;
     cargo: string;
     data_nascimento: Date;
+    // Método construtor da classe Funcionario
     constructor(nome: string, email: string, telefone: string, cargo: string, data_nascimento: Date) {
         this.cargo = cargo;
         this.email = email;
@@ -14,10 +14,22 @@ class Funcionario {
         this.nome = nome;
         this.data_nascimento = data_nascimento;
     }
+    mostrarDados() {
+        console.log('--------------Dados do funcionario-------------')
+        console.log(`Nome do usuario___: ${this.nome}`)
+        console.log(`E-mail____________: ${this.email}`)
+        console.log(`Telefone__________: ${this.telefone}`)
+        console.log(`Cargo_____________: ${this.cargo}`)
+        console.log(`Data de nascimento: ${this.data_nascimento.toLocaleDateString('pt-BR')}`)
+        console.log('------------------------------------------------')
+    }
+
 }
 
 const funcionario1 = new Funcionario('Fulano', 'fulano@gmail.com', "1234567890", 'medico', new Date("2023-2-23"))
-console.log(funcionario1)
+console.log(funcionario1.mostrarDados())
+
+
 class Paciente {
     nome: string;
     cpf: string;
@@ -55,9 +67,21 @@ class Consulta {
         this.convenio = convenio;
         this.paciente = paciente;
     }
+    mostrarDados() {
+        console.log(`Funcionario: 
+            1 - ${funcionario1.nome} \n
+            2 - ${funcionario1.nome} \n
+            3 - ${funcionario1.nome} \n
+            4 - ${funcionario1.nome} \n
+            5 - ${funcionario1.nome} \n
+            `)
+    }
 }
 
 const consulta1 = new Consulta('Mosquito', new Date('2024-09-23T12:12:12'), true, funcionario1, paciente1)
+const consulta2 = new Consulta('Japao', new Date('2024-09-23T12:12:12'), false, funcionario1, paciente1)
 
 console.log(consulta1)
+console.log(consulta2)
+
 
